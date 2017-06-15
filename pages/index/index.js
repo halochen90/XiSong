c937e7e3671419aae5b54e1e92893ff464132e2a
Halo Chen <halochen90@gmail.com>
2017-06-15 15:19:33 +0800

    首页图片展示

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,9 @@
 var app = getApp()
 Page({
   data: {
-    userInfo: {}
+    userInfo: {},
+    preUrl:"http://image.halochen.com/",
+    recordPage:{}
   },
   //事件处理函数
   onLoad: function () {
@@ -14,6 +16,26 @@ Page({
       that.setData({
         userInfo:userInfo
       });
-    })
+    }),
+    //获取第一页的10条记录
+      sendRequest(1,that);
   }
 })
+
+//发送请求获取记录
+function sendRequest(currentIndex,that) {
+  wx.request({
+    url: 'https://api.halochen.com:8443/api/records/currentIndex/'+ currentIndex,
+    method: 'GET',
+    data: {},
+    header: {
+      'content-type': 'application/json'
+    },
+    success: function (res) {
+     that.setData({
+       recordPage:res.data
+     })
+     console.log(that.data.recordPage);
+    }
+  })
+}
