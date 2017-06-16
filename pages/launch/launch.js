4d3bc7281a754f83584d91011be51338459c52fb
Halo Chen <halochen90@gmail.com>
2017-06-16 16:02:17 +0800

    启动页增加计时

--- pages/launch/launch.js
+++ pages/launch/launch.js
@@ -6,7 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    birthday:'2017-6-1',
+    dayNum:0
   },
 
   /**
@@ -20,6 +21,12 @@ Page({
       that.setData({
         userInfo: userInfo
       });
+    })
+
+    //获取破壳天数
+    var dayNum = dateDiff(that.data.birthday,new Date().toDateString());
+    that.setData({
+      dayNum:dayNum
     })
   },
   watchPhoto: function(){
@@ -41,3 +48,12 @@ Page({
   
   }
 })
+function dateDiff(sDate1, sDate2) {    //sDate1和sDate2是2002-12-18格式  
+  var aDate, oDate1, oDate2, iDays
+  aDate = sDate1.split("-")
+  oDate1 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0])    //转换为12-18-2002格式  
+  aDate = sDate2.split("-")
+  oDate2 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0])
+  iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24)    //把相差的毫秒数转换为天数  
+  return iDays
+}
