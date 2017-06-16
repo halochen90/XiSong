cd58edc9ac37c327f9c8cc9d53f4fac33dd1db9b
Halo Chen <halochen90@gmail.com>
2017-06-16 15:41:51 +0800

    添加启动页

---
+++ pages/launch/launch.js
@@ -0,0 +1,43 @@
+// launch.js
+var app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      });
+    })
+  },
+  watchPhoto: function(){
+    var that = this;
+    console.log(that.data.userInfo)
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
