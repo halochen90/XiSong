278534d634f073d30ceb378b6d788a6279432eeb
halo <halochen90@gmail.com>
2017-06-16 11:23:00 +0800

    启动的时候登录

--- app.js
+++ app.js
@@ -2,24 +2,28 @@
 App({
   REQUEST_URL: "https://request.halochen.com",
   onLaunch: function () {
-   
+    var that = this
+    //调用登录接口
+    wx.login({
+      success: function (result) {
+        if (result.errMsg == 'login:ok') {
+          console.log(result.code);
+        }
+        wx.getUserInfo({
+          success: function (res) {
+            that.globalData.userInfo = res.userInfo
+            typeof cb == "function" && cb(that.globalData.userInfo)
+          }
+        })
+      }
+    })
   },
   getUserInfo:function(cb){
     var that = this
     if(this.globalData.userInfo){
       typeof cb == "function" && cb(this.globalData.userInfo)
     }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
-      })
+      console.log("登录态获取失败")
     }
   },
   globalData:{
--- pages/launch/launch.js
+++ pages/launch/launch.js
@@ -16,19 +16,19 @@ Page({
   onLoad: function (options) {
     var that = this;
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
-      //更新数据
-      that.setData({
-        userInfo: userInfo
-      });
-    })
+    // app.getUserInfo(function (userInfo) {
+    //   //更新数据
+    //   that.setData({
+    //     userInfo: userInfo
+    //   });
+    // })
 
     //获取破壳天数
     var d = new Date();
     var today = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
 
     var num = dateDiff(that.data.birthday, today);
-    console.log("num:" + num)
+
     that.setData({
       days: num
     })
